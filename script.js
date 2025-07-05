f0628a0a718b2796253626e15dd6bc676c5a515f
aadicodesforfun <adityakumartripathi6285@gmail.com>
2025-07-05 10:38:40 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,86 @@
+const introVideo = document.querySelector('.intro');
+const main = document.querySelector('#main');
+
+document.addEventListener('mousemove', function(dets) {
+    gsap.to('#cursor', {
+        x: dets.x - 5,
+        y: dets.y - 5,
+        duration: 0.6,
+    })
+})
+
+function mainPageAppears(){
+    introVideo.style.display = "none";
+    main.style.display = "block";
+    nav.style.display = "flex";
+}
+
+main.style.display = 'block' // remove this to play the intro video
+
+let tl = gsap.timeline();
+
+// tl.to(introVideo, {
+//     delay: 1.5,
+//     duration: 6,
+//     onStart: () => {
+//         introVideo.play();
+//     }
+// })
+
+// tl.to(introVideo, {
+//     opacity: 0,
+//     duration: 1,
+//     onComplete: mainPageAppears,
+// })
+
+tl.from('#main', {
+    filter: "blur(10px)",
+    duration: 1.5, 
+    opacity: 0,
+})
+
+tl.from('nav', {
+    opacity: 0,
+    y: -40,
+    duration: 1,
+})
+
+tl.from('.left',{
+    y: 40,
+    duration: 1, 
+    opacity: 0,
+},"-=0.5")
+
+tl.from('.middle li', {
+    y: 40, 
+    duration: 1, 
+    opacity: 0,
+    stagger: 0.2,
+},"-=1")
+
+tl.from('.right .search', {
+    duration: 1,
+    opacity: 0,
+}, "-=1")
+
+tl.from('.right i',{
+    y:40, 
+    duration: 1,
+    opacity: 0,
+    stagger: 0.2,
+},"-=1")
+
+tl.from('.page1 h1', {
+    x: 200,
+    opacity: 0,
+    duration: 1.5,
+    ease: 'bounce.out'
+})
+
+gsap.from('.page1 .view-more', {
+    scale: 0.5,
+    // x: -400,
+    opacity: 0,
+    duration: 1,
+    delay: 2, // make it 10 when the intro video is present or else 2
+})
